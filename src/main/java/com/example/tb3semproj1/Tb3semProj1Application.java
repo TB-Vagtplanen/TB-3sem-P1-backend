5adf812b59a84ced656b40d939b3f3314f00aacd
Mark <mark771e@stud.kea.dk>
2023-03-16 12:46:38 +0100

    git add project

---
+++ src/main/java/com/example/tb3semproj1/Tb3semProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.tb3semproj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tb3semProj1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tb3semProj1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tb3semproj1/Tb3semProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tb3semproj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tb3semProj1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
