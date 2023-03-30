df73fd6409c76f73598a4477294ec117c762f13b
Mark <mark771e@stud.kea.dk>
2023-03-30 12:47:19 +0200

    added authority check

--- src/main/java/dat3/vagtplan/api/UserController.java
+++ src/main/java/dat3/vagtplan/api/UserController.java
@@ -5,6 +5,7 @@ import dat3.vagtplan.dto.UserRequest;
 import dat3.vagtplan.dto.UserResponse;
 import dat3.vagtplan.service.UserService;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,38 +23,44 @@ public class UserController {
 
 
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     List<UserResponse> getUsers() {
         return userService.getUsers();
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     UserResponse addUser(@RequestBody UserRequest userRequest) {
         return userService.addUser(userRequest);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/{username}")
     UserResponse findUser(@PathVariable String username) {
         return userService.findUser(username);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping()
     UserResponse editUser(@RequestBody UserRequest body) {
         return userService.editUser(body);
     }
 
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{username}")
     Boolean deleteUser(@PathVariable String username) {
         return userService.deleteUser(username);
     }
 
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PatchMapping("/enabled/{username}")
     Boolean flipEnabled(@PathVariable String username) {
         return userService.flipEnabled(username);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PatchMapping("/enabled/{username}/{value}")
     Boolean switchEnabled(@PathVariable String username, @PathVariable boolean value) {
         return userService.switchEnabled(username,value);
